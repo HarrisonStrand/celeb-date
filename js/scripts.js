4dd7a549edb295522243e4d41dfed854c7b02d74
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-22 14:55:46 -0700

    Add branch to show shadow class after input.
    Co-Authored By:Harrison Strand <harrisonstrand@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
-const yourTypeStr = $("input:radio[name=your-type]:checked").val();
-const yourType = parseInt(yourTypeStr);
-
-const nightStr = $("input:radio[name=night]:checked").val();
-const night = parseInt(nightStr);
-
-const generationStr = $("input:radio[name=generation]:checked").val();
-const generation = parseInt(generationStr);
-  
 $(document).ready(function() {
   $("button#submit").click(function() {
+    
+    const yourTypeStr = $("input:radio[name=your-type]:checked").val();
+    const yourType = parseInt(yourTypeStr);
+    
+    const nightStr = $("input:radio[name=night]:checked").val();
+    const night = parseInt(nightStr);
+    
+    const generationStr = $("input:radio[name=generation]:checked").val();
+    const generation = parseInt(generationStr);
+    
     const result = yourType + night + generation;
+    
     console.log(result);
+    
+    
+    
+    
+    
+    
+    
+    if (result < 5 >=3) {
+      $("#shadow").show();
+    }    
+    event.preventDefault();
   });
 });
