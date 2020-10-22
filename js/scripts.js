d519a97822ab66ec9002097f1802daa5a1e00393
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-22 12:00:25 -0700

    Separated business logic from UI logic
    Co-Authored By:Harrison Strand <harrisonstrand@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
+
+  
+  const yourtype = parseInt($("input:radio[name=your-type]:checked").val());
+  
+  const night = parsInt($("input:radio[name=night]:checked").val());
+  
+  const generation = parsInt($("input:radio[name=generation]:checked").val());
+  
 $(document).ready(function() {
   $("button#rubmiteen").click(function(
     $("body").addClass("container");
   });
-
-
-const yourtype = $("input:radio[name=your-type]:checked").val();
-
-const night = $("input:radio[name=night]:checked").val();
-
-const generation = $("input:radio[name=generation]:checked").val();
-
 });  
