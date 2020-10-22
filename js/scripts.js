55354d6fb07d4e082b90e9dc56b2729cb8ad6ee0
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-22 11:50:17 -0700

    Added images to be used, continued setup of radio buttons
    Co-Authored By:Harrison Strand <harrisonstrand@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("button#rubmiteen").click(function(
+    $("body").addClass("container");
+  });
+
+
+const yourtype = $("input:radio[name=your-type]:checked").val();
+
+const night = $("input:radio[name=night]:checked").val();
+
+const generation = $("input:radio[name=generation]:checked").val();
+
+});  
